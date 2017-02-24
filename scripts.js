91ee07807b4c721389edfb2b064319f585fb8ce2
atauf <ataufmann@gmail.com>
2017-02-24 09:48:18 -1000

    Added RESET button

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
+	//create 256 (16x16) divs
 	for (var i = 0; i < 16; i++){
 		for (var y = 0; y < 16; y++){
 			$('.container').append('<div class="block"></div>');
 		};
 	};
 	
+	//Grey out the blocks that your mouse passes enters
 	$('.block').on('mouseenter', function(){
 		$(this).addClass('entered');
+	});
+
+	//Give RESET button ability to reset
+	$('button').on('click', function(){
+		$('.entered').removeClass('entered');
 	});
 });
