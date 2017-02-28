2d55642fabe1c6d235a11d14d0950dab7811dec3
atauf <ataufmann@gmail.com>
2017-02-27 15:59:51 -1000

    Add custom number of squares functionality is GOOD!   - Could still improve by making sure there are always the same number of rows and columns

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 	//create 256 (16x16) divs by default
-/*	for (var i = 0; i < 16; i++){
+	for (var i = 0; i < 16; i++){
 		for (var y = 0; y < 16; y++){
 			$('.container').append('<div class="block"></div>');
 		};
 	};
-*/	
+	
 
 	//Give form ability to take user input and create # of <div>s accordingly
 	$('#input-button').on('click', function(){
+		$(".block").remove();
 		var $input = +$('#input-grid-size').val();
 		//alert("input: " + $input + " " + typeof($input));
 		if ($input > 0){
@@ -26,7 +27,8 @@ $(document).ready(function(){
 			};
 		
 		}
-		
+	});
+
 	//Grey out the blocks that your mouse passes enters
 	$('.block').on('mouseenter', function(){
 		$(this).addClass('entered');
@@ -38,8 +40,3 @@ $(document).ready(function(){
 	});
 
 	});
-
-
-
-
-});
